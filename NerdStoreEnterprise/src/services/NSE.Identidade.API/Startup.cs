878e92f694494c7484f3fed17d9d0a2e0ced783c
rafael machado <rafael.machado@genialinvestimentos.com.br>
2020-08-27 14:24:59 -0300

    - inclusão de um banco local para teste - inclusão do Context do banco - inclusão de configuração do appsettguis - inclusão da chamada do DBContext ity na Startup - inclusão do Identity no Startap

--- NerdStoreEnterprise/src/services/NSE.Identidade.API/Startup.cs
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Startup.cs
@@ -5,11 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NSE.Identidade.API.Data;
 
 namespace NSE.Identidade.API
 {
@@ -25,6 +28,14 @@ namespace NSE.Identidade.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDBContext>(options =>
+              options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>() //é o usuario padrão do identity
+                    .AddRoles<IdentityRole>() // as roles do identity
+                    .AddEntityFrameworkStores<ApplicationDBContext>() // o banco que criamos o Identity
+                    .AddDefaultTokenProviders(); // não tem nada haver com JWT. é o token dele para resetar senha, autenticar conta recem criada
+                                                 // praticamente uma criptografia dentro de um token para te reconhecer
             services.AddControllers();
         }
 
@@ -41,6 +52,8 @@ namespace NSE.Identidade.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
