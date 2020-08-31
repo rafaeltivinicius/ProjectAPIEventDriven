84914e878405d7654ae0f350895aa27ec9a5936c
rafael machado <rafael.machado@genialinvestimentos.com.br>
2020-08-30 23:23:53 -0300

    - remoção migration - inclusão de acesso do Banco de Catalogo API - inclusão de AggregateRoot para NSE.Core

---
+++ EventDriven/src/buildBlocks/NSE.Core/DomainObjects/IAggregateRoot.cs
@@ -0,0 +1,20 @@
+﻿namespace NSE.Core.DomainObjects
+{
+    /*
+     vc tem uma entidade, mas ela pode ser filha de outra entidade e elas são persistidas juntas (tratadas como uma)
+      a ideia é ter uma marcação, para dizer que tal classe, é uma AggregateRoot
+
+     Um AGREGADO é um agrupamento de objetos associados que tratamos como uma unidade para fins de alterações 
+    de dados. Cada AGREGADO possui uma raiz e um limite. O limite define o que está dentro do 
+    AGREGADO. A raiz é uma ENTIDADE única e específica contida no AGREGADO.A raiz é o único membro do AGGREGATE que objetos 
+    externos podem conter referências
+
+    Um exemplo é um modelo contendo uma Customerentidade e uma Addressentidade. Nunca acessaríamos uma Addressentidade 
+    diretamente do modelo, pois não faria sentido sem o contexto de um associado Customer. Então poderíamos dizer 
+    que Customere Addressjuntos formam um agregado e que Customeré uma raiz agregada.
+     */
+
+    public interface IAggregateRoot
+    {
+    }
+}
---
+++ EventDriven/src/services/NSE.Catalogo.API/Data/CatalogoContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Models;
+using NSE.Core.Data;
+using System.Threading.Tasks;
+
+namespace NSE.Catalogo.API.Data
+{
+    public class CatalogoContext : DbContext, IUnitOfWork
+    {
+        public CatalogoContext(DbContextOptions<CatalogoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            return await base.SaveChangesAsync() > 0;
+        }
+
+    }
+
+}
+
